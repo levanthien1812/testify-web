f6411ca7a211f458a44b20d924979a612cbb7968
Le Van Thien <20521947@gm.uit.edu.vn>
2024-06-30 18:56:58 +0700

    top takers and user photo

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import RecentTests from "./components/RecentTests";
+import TopTakers from "./components/TopTakers";
+import { useSelector } from "react-redux";
+import { RootState } from "../../stores/rootState";
 
 const HomePage = () => {
+    const user = useSelector((state: RootState) => state.auth.user);
+
     return (
         <div className="xl:w-2/3 md:w-5/6 mx-auto py-10">
             <RecentTests />
+            {user?.role === "maker" && <TopTakers />}
         </div>
     );
 };
